0b94db8ce873ddab7eea5c4962475f730eb9fdac
Pegah Eizad <peizadkh@gmail.com>
2023-01-02 13:10:20 -0500

    renamed file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import LandingPage from "./components/LandingPage";
-import RestaurantPage from "./components/RestaurantPage";
+import RestaurantDetails from "./components/RestaurantDetails";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/search/restaurants/:id" element={<RestaurantPage />} />
+        <Route path="/search/restaurants/:id" element={<RestaurantDetails />} />
         <Route path="/search/resturants" element={<LandingPage />} />
         <Route
           path="/"
