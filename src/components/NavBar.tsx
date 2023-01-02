bc84e3f112cc125493ec3782bf0605f7dee281b5
Pegah Eizad <peizadkh@gmail.com>
2023-01-01 22:20:14 -0500

    some navbar styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,34 @@ import "./NavBar.css";
 export default function navbar() {
   return (
     <div className="nav-bar" id="example-menu">
-      <title className="top-bar-left">ushi: Yelp but for sushi</title>
-      <div className="top-bar-right">
+      <div className="top-bar-left">
+        <img
+          alt={"site logo placeholder"}
+          className={"logo-image"}
+          src={"https://cdn-icons-png.flaticon.com/512/3014/3014482.png"}
+        />
+        {/* <ul className="menu">
+          <li className="search-input-li-wrapper">
+            <label className="search-label">
+              Search sushi restaurants in Minneapolis
+            </label>
+            <input
+              className="search-input"
+              type="search"
+              placeholder="Sushi restaurants in Minneapolis..."
+            />
+          </li>
+        </ul> */}
+        {/* <h1 className="nav-bar-title">Sushi restaurants in Minneapolis...</h1> */}
+      </div>
+      {/* <div className="top-bar-right">
         <ul className="menu">
-          <li>
-            <input type="search" placeholder="Search" />
+          <li className="search-input-li-wrapper">
+            <input
+              className="search-input"
+              type="search"
+              placeholder="Search sushi restaurants in Minneapolis..."
+            />
           </li>
           <li>
             <button
@@ -19,7 +42,8 @@ export default function navbar() {
             </button>
           </li>
         </ul>
-      </div>
+      </div> */}
+      {/* <div className="empty-div"></div> */}
     </div>
   );
 }
